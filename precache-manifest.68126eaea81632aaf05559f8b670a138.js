bdaa042236d85789c3f70f17e37129b6090b9918
Simon Gschnell <Simon.Gschnell@stud-inf.unibz.it>
2020-04-15 19:54:09 +0200

    Updates

---
+++ precache-manifest.68126eaea81632aaf05559f8b670a138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e27d098d8861b71b35062fb386d8f8c",
+    "url": "/Robos/index.html"
+  },
+  {
+    "revision": "7f5eb5f86939835dd4ae",
+    "url": "/Robos/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7f5eb5f86939835dd4ae",
+    "url": "/Robos/static/js/2.b6642ff5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Robos/static/js/2.b6642ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61cbdc09486c77c215e3",
+    "url": "/Robos/static/js/main.91f9f29b.chunk.js"
+  },
+  {
+    "revision": "4a4d2e0883ccdeaa04f7",
+    "url": "/Robos/static/js/runtime-main.a5e6f977.js"
+  }
+]);
